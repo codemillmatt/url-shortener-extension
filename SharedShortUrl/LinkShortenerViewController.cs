95fcf5fd1de7cbdfec0b14f73e9047b7d02cbde0
Matt Soucoup <msoucoup@codemilltech.com>
2018-02-26 05:58:45 -0600

    Custom UI start

---
+++ SharedShortUrl/LinkShortenerViewController.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace SharedShortUrl
+{
+    public partial class LinkShortenerViewController : UIViewController
+    {
+        public LinkShortenerViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
